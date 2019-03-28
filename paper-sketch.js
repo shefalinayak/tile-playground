e0afcb7559b79bd5e1c79f50ca4595068fafdc85
Shefali Nayak <nayakshefali@gmail.com>
2019-03-27 23:35:26 -0400

    protoTriangle

--- paper-sketch.js
+++ paper-sketch.js
@@ -1,10 +1,27 @@
-// Create a Paper.js Path to draw a line into it:
-var path = new Path();
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
+
+var sl = 100;
+var origin = new Point(0,0);
+var endpt = new Point(sl,0);
+
+var protoEdge = new Path();
+protoEdge.strokeColor = 'black';
+protoEdge.add(origin, endpt);
+protoEdge.pivot = origin;
+protoEdge.insert(1, new Point(50,0));
+protoEdge.insert(2, new Point(70,-10));
+protoEdge.insert(3, new Point(75,0));
+
+var e1 = protoEdge.clone();
+var e2 = e1.clone();
+e2.rotate(-60,endpt).reverse();
+var e3 = e2.clone();
+e3.rotate(-60).reverse();
+
+var protoTriangle = e1;
+protoTriangle.join(e2);
+protoTriangle.join(e3);
+
+protoTriangle.fillColor = 'paleturquoise';
+
+protoTriangle.translate(50,100);
+protoEdge.translate(50,50);
